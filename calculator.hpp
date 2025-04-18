269068c340040cb4c6c8aca4579687428b492817
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2025-04-17 22:43:24 -0500

    Update calculator.hpp

--- calculator.hpp
+++ calculator.hpp
@@ -2,9 +2,16 @@
 #define __CALCULATOR_HPP
 
 #include <string>
+#include <vector>
 
 // this function should expect that `expression` has already been
 // validated and thus throw no exceptions
 double parse_number(const std::string &expression);
+
+bool isValidDouble(const std::string &expression);
+
+std::string addStrings(const std::string &num1, const std::string &num2);
+
+std::string normalizeNumberString(const std::string& num);
 
 #endif // __CALCULATOR_HPP
